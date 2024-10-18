1b3074c03d3e745613605c028eaf363e32b4ff65
Artyom Nikiforov <dom2411edinstvo@gmail.com>
2024-10-19 00:58:16 +0500

    first commit

--- counter.js
+++ counter.js
@@ -2,24 +2,29 @@ const counterEl = document.querySelector('[data-find="counterValue"]');
 const addOnePoolBtn = document.getElementById("addOnePoolBtn");
 const addTwoPoolsBtn = document.getElementById("addTwoPoolsBtn");
 const resetBtn = document.getElementById("resetBtn");
-// counterEl = 1
 
 const COUNTER_INITIAL_VALUE = 0;
 
 let counter = COUNTER_INITIAL_VALUE;
-counterEl.innerText = counter;
+updateCounterDisplay();
+
+function updateCounterDisplay() {
+    counterEl.innerText = counter;
+}
 
 addOnePoolBtn.addEventListener('click', function () {
-    counter = counter + parseInt(addOnePoolBtn.dataset.pools);
-    counterEl.innerText = counter;
+    const poolsToAdd = parseInt(addOnePoolBtn.dataset.pools) || 0;
+    counter += poolsToAdd;
+    updateCounterDisplay();
 });
 
 addTwoPoolsBtn.addEventListener('click', function () {
-    counter = counter + parseInt(addTwoPoolsBtn.dataset.pools);
-    counterEl.innerText = counter;
+    const poolsToAdd = parseInt(addTwoPoolsBtn.dataset.pools) || 0;
+    counter += poolsToAdd;
+    updateCounterDisplay();
 });
 
 resetBtn.addEventListener('click', function () {
     counter = COUNTER_INITIAL_VALUE;
-    counterEl.innerText = counter;
+    updateCounterDisplay();
 });
