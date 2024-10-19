a8e794e8f09874d901e010912f40579c7027604c
Artyom Nikiforov <dom2411edinstvo@gmail.com>
2024-10-19 17:43:33 +0500

    last commit

--- counter.js
+++ counter.js
@@ -1,4 +1,4 @@
-const counterEl = document.querySelector('[data-find="counterValue"]');
+const counterEl = document.getElementById("counter-text");
 const addOnePoolBtn = document.getElementById("addOnePoolBtn");
 const addTwoPoolsBtn = document.getElementById("addTwoPoolsBtn");
 const resetBtn = document.getElementById("resetBtn");
@@ -6,51 +6,19 @@ const resetBtn = document.getElementById("resetBtn");
 const COUNTER_INITIAL_VALUE = 0;
 
 let counter = COUNTER_INITIAL_VALUE;
-updateCounterDisplay();
-
-function updateCounterDisplay() {
-    counterEl.innerText = counter;
-}
-
-function animateCounter(target, duration = 500) {
-    // Блокируем кнопки перед началом анимации
-    addOnePoolBtn.disabled = true;
-    addTwoPoolsBtn.disabled = true;
-    resetBtn.disabled = true;
-
-    const start = counter;
-    const change = target - start;
-    const startTime = performance.now();
-
-    const animate = (currentTime) => {
-        const elapsedTime = currentTime - startTime;
-        const progress = Math.min(elapsedTime / duration, 1);
-        counter = Math.floor(start + change * progress);
-        updateCounterDisplay();
-
-        if (progress < 1) {
-            requestAnimationFrame(animate);
-        } else {
-        //Отменяем блокировку кнопок по окончании анимации
-        addOnePoolBtn.disabled = false;
-        addTwoPoolsBtn.disabled = false;
-        resetBtn.disabled = false;
-        }
-    };
-
-    requestAnimationFrame(animate);
-}
+counterEl.innerText = counter;
 
 addOnePoolBtn.addEventListener('click', function () {
-    const poolsToAdd = parseInt(addOnePoolBtn.dataset.pools) || 0;
-    animateCounter(counter + poolsToAdd);
+    counter++;
+    counterEl.innerText = counter;
 });
 
 addTwoPoolsBtn.addEventListener('click', function () {
-    const poolsToAdd = parseInt(addTwoPoolsBtn.dataset.pools) || 0;
-    animateCounter(counter + poolsToAdd);
+    counter = counter +2;
+    counterEl.innerText = counter;
 });
 
 resetBtn.addEventListener('click', function () {
-    animateCounter(COUNTER_INITIAL_VALUE);
+    counter = COUNTER_INITIAL_VALUE;
+    counterEl.innerText = counter;
 });
